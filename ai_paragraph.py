4ccada9240ed38d7d0692c5ad9bdf4e7905fd45d
dummyre <xomicremag@gmail.com>
2026-08-19 19:00:03 -0400

    Adding in the beginning of the ai paragraph python file.

---
+++ ai_paragraph.py
@@ -0,0 +1,8 @@
+from llama_cpp import Llama
+
+msgs = [
+    {
+        "role": "system",
+        "content": "You are an information supplier. You are provided the name of a species of plant and you output information on that species"
+    }
+]

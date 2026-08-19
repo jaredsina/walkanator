7da4ce05ee1a466d2689dec23682e4d43db0a53d
Shrey719 <shrey150436@gmail.com>
2026-08-19 19:38:28 -0400

    its allivveeee

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+
+
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+      <div style={{
+        width: "100%",
+        height: "100%",
+        background: "pink"
+      }}>
+      </div>
+  );
+}

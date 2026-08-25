bc6d4dd4ff5183c4df2188fa1b1e8058761044d0
Shrey719 <shrey150436@gmail.com>
2026-08-25 18:57:42 -0400

    add tracking for location & scaffold bottom elements

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,69 @@
 
-
+import { Colors } from "@/constants/theme";
+import Geolocation from '@react-native-community/geolocation';
+import type { CSSProperties } from "react";
 import React from "react";
 
+
+//....
+function getLocation(): Promise<String> {
+  return new Promise((resolve, reject) => {
+    Geolocation.getCurrentPosition(
+      (info) => {
+        resolve(`${info.coords.latitude}, ${info.coords.longitude}`);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+}
+
 export default function HomeScreen() {
+
+
+  // defo not how youre 'supposed' to do this 
+  // but uh idc
+  const main: CSSProperties = {
+      width: "100%",
+      height: "100%",
+      background: Colors.background,
+      color: Colors.text,
+  }
+
+  const head: CSSProperties= {
+    textAlign: "center"
+  }
+  // placeholder :: just a black square for now
+  const iMap: CSSProperties = {
+    background: "black",
+    width: "80vmin",
+    height: "40vh",
+    margin: "auto",
+    marginTop: "5%",
+  }
+  
+
   return (
-      <div style={{
-        width: "100%",
-        height: "100%",
-        background: "pink"
-      }}>
+      <div style={main}>
+        <div style={head}>
+          <h1 style={{fontSize:"3em", marginBottom: "0"}}>
+            Almanac
+          </h1>
+          <span style={{fontSize: "1.5em"}}>Sign in</span>
+        </div>
+
+        <div style={{textAlign:"center"}}>
+          <div style={iMap}></div>
+          <span style={{fontSize: "1.5em"}}>Location: {getLocation()}</span>
+        </div>
+
+        <div style={{}}>
+          Corner stuff
+        </div>
+
       </div>
   );
 }
+
+
--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -4,27 +4,17 @@
  */
 
 import '@/global.css';
-
 import { Platform } from 'react-native';
 
 export const Colors = {
-  light: {
-    text: '#000000',
-    background: '#ffffff',
-    backgroundElement: '#F0F0F3',
-    backgroundSelected: '#E0E1E6',
-    textSecondary: '#60646C',
-  },
-  dark: {
-    text: '#ffffff',
-    background: '#000000',
-    backgroundElement: '#212225',
-    backgroundSelected: '#2E3135',
-    textSecondary: '#B0B4BA',
-  },
+    text: '#ff6366',
+    background: '#386641',
+    backgroundElement: '#6a994e ',
+    backgroundSelected: '#a7c957',
+    textSecondary: '#f2e8cf',
 } as const;
 
-export type ThemeColor = keyof typeof Colors.light & keyof typeof Colors.dark;
+export type ThemeColor = keyof typeof Colors;
 
 export const Fonts = Platform.select({
   ios: {

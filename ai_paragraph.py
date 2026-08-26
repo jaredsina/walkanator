177d9046afdebe97ab07e83818532b0cad373558
clubdirt <jai.m.seth@gmail.com>
2026-08-26 18:10:13 -0400

    Adding in AI script that can generate paragraphs about thing.

--- ai_paragraph.py
+++ ai_paragraph.py
@@ -1,8 +1,31 @@
-from llama_cpp import Llama
+"""from llama_cpp import Llama
 
-msgs = [
+llm = Llama(
+    model="minicpm-v4.6:1b",
+    chat_format="chatml"
+    msgs = [
     {
         "role": "system",
-        "content": "You are an information supplier. You are provided the name of a species of plant and you output information on that species"
+        "content": "You are an information supplier. You are provided the name of a species of plant and you output information on that species."
     }
 ]
+
+)
+"""
+from ollama import chat
+
+response = chat(
+    
+    model="minicpm-v4.6:1b",
+    messages = [
+        {
+            "role":"user",
+            "content":"what is a llama, write a paragraph?"
+        }
+    ]
+    
+)
+
+print(response.message.content)
+
+
